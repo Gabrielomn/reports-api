f0968e2e5cfd1fdf61eb3036a9e1c8b7cb9a949f
gabrielomn <gabriel.nobrega@ccc.ufcg.edu.br>
2019-08-14 21:11:04 -0300

    adding cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const cors = require('cors')
 const app = express()
-
+app.use(cors())
+app.options('*', cors())
 require('./router.js')(app)
 const PORT = process.env.PORT || 8080
 app.listen(PORT, console.log(`Server is listening on port ${PORT}`))

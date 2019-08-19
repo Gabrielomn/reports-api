9220d4b19ee64a28dc00f70cf603c9b365623c59
gabrielomn <gabriel.nobrega@ccc.ufcg.edu.br>
2019-08-19 17:34:01 -0300

    increasing token duration

--- services/authenticate.js
+++ services/authenticate.js
@@ -8,7 +8,7 @@ router.post('/', (req, res) => {
         return res.status(401).json("Invalid PIN")
     }
     const token = jwt.sign({ pin: pin }, authConfig.secret, {
-        expiresIn: 30
+        expiresIn: 600
     })
 
     if(token){
